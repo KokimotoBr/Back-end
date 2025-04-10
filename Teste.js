13f53ad5397188a1023116351886296bc8ff6881
KokimotoBr <pohlmann.anthony@gmail.com>
2025-04-10 10:10:38 -0400

    Testes que eu fiz

---
+++ Teste.js
@@ -0,0 +1,53 @@
+"Os tipos de variáveis"
+
+//Var, let e const.
+
+//var; não é mais utilizada, mas pode ser alterada que nem a "let".
+
+//let; um valor que pode ser alterado ao longo do código.
+
+//const; não pode ser alterada.
+
+"Tipos de dados"
+
+//Number; Números.
+
+//Boolean; True or False.
+
+//String; Palavras.
+
+"Como saber o tipo de uma variável?"
+
+//typeof
+
+"O array"
+
+//isso é uma array:
+
+let estados = ["Rio de Janeiro", "São paulo", "Bahia"];
+
+//um array pode ter os 3 tipos de dados, mas só no JavaScript.
+
+let linguagens = ["JavaScript", "PHP", "Java", "C#"];
+
+console.log(linguagens.length);
+
+//Vai imprimir 4.
+
+"Null"
+
+//null é um valor sem tipo: não é string, bool nem number.
+
+let nome = null;
+console.log(nome);
+
+//vai imprimir: null.
+
+"const"
+
+//não deixa alterar o valor.
+
+const aula = "JavaScript";
+aula = "Js";
+
+//Vai da erro
