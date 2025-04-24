a6e9adc441205cc5c26420363d3d3f4ac0a31e45
ANTHONY POHLMANN RODRIGUES <pohlmann.anthony@gmail.com>
2025-04-24 11:47:18 -0400

    Add files via upload

--- Exercicios_operadores.js
+++ Exercicios_operadores.js
@@ -1,31 +1,28 @@
 //1. Crie um programa que declare duas variáveis e exiba o resultado da soma, subtração, multiplicação e divisão desses números.
 
-n1 = 6;
-n2 = 2;
-soma = (n1 + n2);
-sub =(n1 - n2);
-mult = (n1 * n2);
-div = (n1 / n2);
-console.log(soma);
-console.log(sub);
-console.log(mult);
-console.log(div);
+let n1 = 6;
+let n2 = 2;
+
+console.log(`${n1} + ${n2} = ${n1+n2}`);
+console.log(`${n1} - ${n2} = ${n1-n2}`);
+console.log(`${n1} * ${n2} = ${n1*n2}`);
+console.log(`${n1} / ${n2} = ${n1/n2}`);
 
 //2. Declare duas idades e utilize operadores de comparação para verificar se uma pessoa é mais velha que a outra.
 
-id1 = 17;
-id2 = 20;
+let id1 = 17;
+let id2 = 20;
 console.log(id1 > id2);
 console.log(id1 < id2);
 
 //3. Crie uma variável idade com um número e use o operador ternário para verificar se o valor armazenado na variável é maior ou igual a 18. Imprima “Maior de idade” ou “Menor de idade”.
 
-id3 = 15;
-aviso = id3 >= 18 ? "Acesso permitido" : "Acesso negado";
+let id3 = 15;
+aviso = id3 >= 18 ? "Maior de idade" : "Menor de idade";
 console.log(aviso);
 
 //4. Crie uma variável com um número e use o operador ternário para verificar se é par ou ímpar.
 
-n3 = 2;
-conta = n3 %2 == 0 ? "Par" : "ímpar";
+let n3 = 2;
+let conta = n3 %2 == 0 ? "Par" : "ímpar";
 console.log(conta);
--- Teste.js
+++ Teste.js
@@ -22,7 +22,8 @@
 
 "O array"
 
-//isso é uma array:
+//array é uma coleção:
+//o array usa [] e não {}.
 
 let estados = ["Rio de Janeiro", "São paulo", "Bahia"];
 
@@ -51,3 +52,200 @@ const aula = "JavaScript";
 aula = "Js";
 
 //Vai da erro
+
+"Operadores"
+
+// + - * / % 
+
+'%'
+
+//retorna o resto de uma divisão
+
+'+'
+
+//Além de somar números, ele tambem pode somar palavras
+
+console.log("5" + "6");
+
+//Vai imprimir 56
+
+'Operadores de atribuição aritmética'
+
+//  X += Y    X = X + Y
+
+'Incremento e decremento'
+
+//Os operadores de incremento ++ e decremento --, são usados para adicionar ou subtrair o valor 1 de uma variável.
+
+var contador = 5;
+contador++;
+console.log(contador);
+
+'Operadores relacionais'
+
+//Igual (==)    Retorna verdadeiro caso os operandos sejam iguais.
+
+//Não igual (!=)    Retorna verdadeiro caso os operandos não sejam iguais.
+
+//Estritamente igual (===)  Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo.
+
+//Estritamente não igual (!==)  Retorna verdadeiro caso os operandos não sejam iguais e/ou não sejam do mesmo tipo.
+
+//Maior que (>)     Retorna verdadeiro caso o operando da esquerda seja maior que o da direita.
+
+//Maior que ou igual (>=)   Retorna verdadeiro caso o operando da esquerda seja maior ou igual ao da direita.
+
+//Menor que (<)     Retorna verdadeiro caso o operando da esquerda seja menor que o da direita.
+
+//Menor que ou igual (<=)   Retorna verdadeiro caso o operando da esquerda seja menor ou igual ao da direita.
+
+console.log(20 == 20); //Vai imprimir True
+
+console.log(119 == 120); //vai imprimir False
+
+console.log("20" == 20); //true
+
+console.log(true == 1); //True
+
+'estritamente igual (===)'
+
+console.log("20" === 20); //false
+
+console.log(true === 1); //false
+
+'Outros operadores'
+
+//AND lógico (&&)   Retorna verdadeiro caso ambos operandos sejam verdadeiros; caso contrário, retorna falso.
+
+//OU lógico (||)    Retorna verdadeiro caso um dos operandos seja verdadeiro; seambos forem falsos, retorna falso.
+
+//NOT lógico (!)    A negação resulta no valor contrário do operador, ou seja, trueserá false e false será true
+
+1 == "1" || "a" == "a" //true
+
+35 === "35" && 1 == 10 //false
+
+!("a" == 1) //true
+
+'Operador ternário'
+
+let idade = 25;
+let mensagem = idade >= 18 ? "Acesso permitido" : "Acesso negado";
+console.log(mensagem);
+
+'Objetos'
+
+var o = new Object(); //cria um objeto vazio: o mesmo que {}.
+var a = new Array(); //Cria um array vazio: o mesmo que [].
+var d = new Date(); //Cria um objeto Date representando a hora atual.
+var r = new RegExp("js"); //Cria um objeto RegEx para comparação de padrões.
+
+class Carro {
+    constructor(marca, modelo){
+        this.marca = marca,
+        this.modelo = modelo;}}
+
+let meuCarro = new Carro('Fiat','Pulse')
+console.log(meuCarro.marca) //'Fiat"
+console.log(meuCarro.modelo) //'Pulse'
+
+let ano = 36
+let meuano = ano
+meuano = 37
+console.log(ano) //36
+
+const carro = {
+    cor: 'azul'}
+
+const outrocarro = carro
+outrocarro.cor = 'amarelo'
+console.log(carro.cor) //'amarelo'
+
+let valor = 10
+let outrovalor = valor
+console.log("Valor: "+valor)
+console.log("outrovalor: " + outrovalor)
+outrovalor = 15
+console.log("Valor: "+valor)
+console.log("outrovalor: " + outrovalor)
+
+const car = {
+    cor : "Azul"
+}
+const bicicleta = car //passagem como referencia -> passa o endereço do objeto na memoria
+console.log("Carro: " + car.cor)
+console.log("Bicicleta: " + bicicleta.cor)
+
+bicicleta.cor = "Verde"
+console.log("Carro: " + car.cor)
+console.log("Bicicleta: " + bicicleta.cor)
+
+//Classe filha
+const carzin = {
+    marca: {
+        nome: 'Ford'
+    },
+    cor: 'azul'
+}
+
+//Coleção de objetos
+
+let disciplina = {
+    id: 3,
+    nome: 'História',
+    carga_horaria: 160,
+};
+
+let coleção_diciplinas = [
+    {id: 1, nome: "Português", carga_horaria: 240}, //indice 0
+    {id: 2, nome: "Matemática", carga_horaria: 220}, //indice 1
+    {id: 3, nome: "História", carga_horaria: 160}, //indice 2
+    {id: 4, nome: "Física", carga_horaria: 120}, //indice 3
+];
+
+console.log(coleção_diciplinas[2]);
+//{id: 3, nome: "História", carga_horaria: 160}
+
+console.log(coleção_diciplinas[6].id); //7
+console.log(coleção_diciplinas[6].nome); //Inglês
+console.log(coleção_diciplinas[6].carga_horaria); //120
+
+'Métodos de objetos'
+
+const carrão = {
+    marca: "Ford",
+    modelo: "Fiesta",
+    ligar: function () {
+        console.log("Ligado");
+    },
+};
+carro.ligar()
+
+const carrinho = {
+    marca: "Ford",
+    modelo: "Fiesta",
+    ligando: function () {
+        console.log(`${this.marca} ${this.modelo} ligando!`);
+    },
+};
+
+carrinho.ligando();
+
+'Classes' //Tem que começarem com letra mascula
+
+class Pessoa{
+    nome;
+}
+const flavio = new Pessoa()
+flavio.nome = 'Flavio'
+flavio.nome
+
+class Pessoinha {
+    nome;
+    saudacao() {
+        return `Olá, meu nome é ${this.nome}`
+    }
+}
+const flavio = new Pessoinha();
+flavio.nome = 'Flavio';
+console.log(flavio.saudacao());
